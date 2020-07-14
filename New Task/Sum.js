d4ec214358d7d3850345305bb20cd0780e2fc118
orpib <orpib@users.noreply.github.com>
2020-07-14 13:22:47 +0600

    Task done

---
+++ New Task/Sum.js
@@ -0,0 +1,42 @@
+let btnCLicked = 0; //reset
+let sum = 0; //reset
+let btnId = 3; //increase
+let colorArray = ["aquamarine", "darksalmon", "khaki"];
+let colorIndex = 2; //reset
+
+function addFunction(id) {
+    let value = document.getElementById(id).innerHTML;
+    btnCLicked++;
+    sum += parseInt(value);
+    if (btnCLicked == 2) {
+        addNewButton(sum);
+    }
+    console.log(id + "clicked")
+    //debugger ;
+}
+function addNewButton(val) {
+    //debugger ;
+    let btn = document.createElement("BUTTON");
+    btn.innerHTML = val;
+    btn.classList.add("btn");
+    btn.id = "btn" + btnId;
+    //btn.onclick = addFunction(btn.id);
+    //debugger ;
+    //$(btn.id).click( addFunction(btn.id));
+    btn.addEventListener("click", function () {
+        addFunction(btn.id);
+    });
+
+    btn.style.backgroundColor = colorArray[colorIndex];
+    colorIndex++;
+    if (colorIndex >= 3)
+        colorIndex = 0;
+
+    sum = 0;
+    btnCLicked = 0;
+    btnId++;
+
+    // btn.var btn = document.createElement("BUTTON"); 
+    //
+    document.getElementById("here").appendChild(btn);
+}
